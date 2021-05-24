533a1f833d7c8ad362d7e12e03015142c65f6d17
Christian Rodriguez <christian.rodriguez@rappi.com>
2021-05-23 22:36:47 -0500

    se eliminar valores en archivo .env

--- script/Backend_Loans.sql
+++ script/Backend_Loans.sql
@@ -1,28 +1,68 @@
+-- public."Clients" definition
 
-/****** Object:  Table [User] ******/
-CREATE TABLE "Users" (
-	id SERIAL PRIMARY KEY,
-	name varchar(200) NOT NULL,
+-- Drop table
+
+-- DROP TABLE public."Clients";
+
+CREATE TABLE public."Clients" (
+	identification int8 NOT NULL,
+	fullname varchar(200) NOT NULL,
+	address varchar(100) NOT NULL,
+	mobile varchar(100) NOT NULL,
+	CONSTRAINT "Clients_pkey" PRIMARY KEY (identification)
+);
+
+
+-- public."Status" definition
+
+-- Drop table
+
+-- DROP TABLE public."Status";
+
+CREATE TABLE public."Status" (
+	id serial NOT NULL,
+	description varchar(10) NOT NULL,
+	CONSTRAINT "Status_pkey" PRIMARY KEY (id)
+);
+
+
+-- public."Types" definition
+
+-- Drop table
+
+-- DROP TABLE public."Types";
+
+CREATE TABLE public."Types" (
+	id serial NOT NULL,
+	"name" varchar(50) NOT NULL,
+	CONSTRAINT "Types_pkey" PRIMARY KEY (id)
+);
+
+
+-- public."Users" definition
+
+-- Drop table
+
+-- DROP TABLE public."Users";
+
+CREATE TABLE public."Users" (
+	id serial NOT NULL,
+	"name" varchar(200) NOT NULL,
 	username varchar(10) NOT NULL,
-	password varchar(200) NOT NULL,
-	creation_date date NOT NULL
+	"password" varchar(200) NOT NULL,
+	creation_date date NOT NULL,
+	CONSTRAINT "Users_pkey" PRIMARY KEY (id)
 );
 
-/****** Object:  Table [Type] ******/
-CREATE TABLE "Types" (
-	id SERIAL PRIMARY KEY,
-	name varchar(50) NOT NULL
-);
 
-/****** Object:  Table [Status] ******/
-CREATE TABLE "Status" (
-	id SERIAL PRIMARY KEY,
-	description varchar(10) NOT NULL
-);
+-- public."Loans" definition
 
-/****** Object:  Table [Loans] ******/
-CREATE TABLE "Loans" (
-	id SERIAL PRIMARY KEY,
+-- Drop table
+
+-- DROP TABLE public."Loans";
+
+CREATE TABLE public."Loans" (
+	id serial NOT NULL,
 	identification_client int4 NOT NULL,
 	borrowed_value numeric NOT NULL,
 	interest_percentage numeric NOT NULL,
@@ -30,38 +70,44 @@ CREATE TABLE "Loans" (
 	pending_value numeric NOT NULL,
 	interest_paid numeric NOT NULL,
 	id_status int4 NOT NULL,
-	creation_date date NOT NULL
+	creation_date date NOT NULL,
+	CONSTRAINT "Loans_pkey" PRIMARY KEY (id),
+	CONSTRAINT fk_loand_clients FOREIGN KEY (identification_client) REFERENCES "Clients"(identification),
+	CONSTRAINT fk_loand_status FOREIGN KEY (id_status) REFERENCES "Status"(id)
 );
 
-/****** Object:  Table [Clients] ******/
-CREATE TABLE "Clients" (
-	identification int8 PRIMARY KEY,
-	fullname varchar(200) NOT NULL,
-	address varchar(10) NOT NULL,
-	mobile varchar(10) NOT NULL
-);
 
-/****** Object:  Table [Payments] ******/
-CREATE TABLE "Payments" (
-	id SERIAL PRIMARY KEY,
+-- public."Payments" definition
+
+-- Drop table
+
+-- DROP TABLE public."Payments";
+
+CREATE TABLE public."Payments" (
+	id serial NOT NULL,
 	id_loan int4 NOT NULL,
 	capital numeric NOT NULL,
 	interest numeric NOT NULL,
 	balance numeric NOT NULL,
 	payment_date date NOT NULL,
-	id_type int4 NOT NULL
+	id_type int4 NOT NULL,
+	CONSTRAINT "Payments_pkey" PRIMARY KEY (id),
+	CONSTRAINT fk_payments_loans FOREIGN KEY (id_loan) REFERENCES "Loans"(id),
+	CONSTRAINT fk_payments_types FOREIGN KEY (id_type) REFERENCES "Types"(id)
 );
 
-/****** Object:  Table [Interests] ******/
-CREATE TABLE "Interests" (
-	id SERIAL PRIMARY KEY,
+
+-- public."Interests" definition
+
+-- Drop table
+
+-- DROP TABLE public."Interests";
+
+CREATE TABLE public."Interests" (
+	id serial NOT NULL,
 	id_loan int4 NOT NULL,
-	share numeric NOT NULL,
-	status varchar(3) NOT NULL
+	"share" numeric NOT NULL,
+	status varchar(3) NOT NULL,
+	CONSTRAINT "Interests_pkey" PRIMARY KEY (id),
+	CONSTRAINT fk_interests_loans FOREIGN KEY (id_loan) REFERENCES "Loans"(id)
 );
-
-alter table "Loans" add constraint FK_loand_status foreign key (id_status) references "Status"(id);
-alter table "Loans" add constraint FK_loand_clients foreign key (identification_client) references "Clients"(identification);
-alter table "Payments" add constraint FK_payments_loans foreign key (id_loan) references "Loans"(id);
-alter table "Payments" add constraint FK_payments_types foreign key (id_type) references "Types"(id);
-alter table "Interests" add constraint FK_interests_loans foreign key (id_loan) references "Loans"(id);
